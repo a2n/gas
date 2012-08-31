ee7108d433a8655601391f184c2c03d761f23142
Alan Lu <a@28int.com>
2012-08-31 17:45:03 +0800

    Update alternativeColor.js, and add _.js.

--- alternativeColor.js
+++ alternativeColor.js
@@ -1,70 +1,56 @@
-/*
-TODOs:
-
-1. Analys and tune performance.
-
-find_mate: 399.0
-changeBackgroundColor: 107.0
-find_mate: 206.0
-changeBackgroundColor: 106.0
-find_mate: 290.0
-changeBackgroundColor: 127.0
-find_mate: 265.0
-changeBackgroundColor: 151.0
-find_mate: 214.0
-changeBackgroundColor: 161.0
-find_mate: 260.0
-changeBackgroundColor: 238.0
-*/
-var ss = null;
-var sheet = null;
-
-function begin() {
-  ss = SpreadsheetApp.getActiveSpreadsheet();
-  var sheets = ss.getSheets();
-  for (var key in sheets) {
-    sheet = sheets[key];
-    checkSheet(sheet);
+var Spreadsheet = function() {
+  // Private
+  function nextMate (cell, row, col) {
+    var nextCell = ss.getRange(row + 1, col).getValue().substr(0, 8);
+    if (nextCell == cell) {
+      nextMate(cell, ++row, col);
+    }  
+    return row;
   }
-}
-
-function checkSheet(sheet) {
-  var col = 4;  
-  var row = 2;
-  while (row < sheet.getLastRow()) {
-    var value = sheet.getRange(row, col).getValue().substr(0, 8);
-    var obj = find_mate(value, row, col);    
-    changeBackgroundColor(row, obj.row);
-
-    // Update loop info.
-    value = obj.next;
-    row = obj.row;
-  }
-}
-
-function find_mate(me, row, col) {  
-  var next = null;
-  while (next = sheet.getRange(++row, col).getValue().substr(0, 8)) {
-    if (me != next) {
-      break;
-    }
-  }
-
+  
+  // eof, private
+  
+  // Public
   return {
-    'row': row,
-    'next': next
-  };
-}
+//@function nextMate(cell, row, col)
+//@brief Mark the same color with the identical prefix cell.
+    alterBackgroundColor : function() {
+      ss = SpreadsheetApp.getActiveSheet();
+      var BEGIN_ROW = 2;
+      var COLUMN = 2;
+      
+      var row = BEGIN_ROW;  
+      var oddArray = new Array(0);
+      var evenArray = new Array(0);
+      var oddDispatch = false;
+      var cell = null;
+  
+      do {
+        cell = ss.getRange(row, COLUMN).getValue().substr(0, 8);
+        var stickyRow = row;    
+        row = nextMate(cell, row, COLUMN);
+        
+        var rowCount = 0;
+        if (row == stickyRow) {
+          rowCount = 1;
+        } else {
+          rowCount = 2;
+        }
+        
+        var beginMarkCol = 6;
+        var colCount = ss.getLastColumn() - beginMarkCol + 1;
+        var rangeNotation = ss.getRange(stickyRow, beginMarkCol, rowCount, colCount).getA1Notation();    
+        oddDispatch ? oddArray.push(rangeNotation) : evenArray.push(rangeNotation);
+        oddDispatch = !oddDispatch;
+      } while (++row <= ss.getLastRow());
+  
+      _.each(evenArray, function(e) {
+        ss.getRange(e).setBackgroundColor("white");
+      });
 
-var color = Boolean(false);
-function changeBackgroundColor(beginRow, endRow) {
-  // Color whole row
-  // Grey:  #f3f3f3
-  // White: #ffffff
-  var colorName = color ? "#ffffff" : "#c3c3c3";
-  var cols = sheet.getLastColumn() - 1;
-  var rows = endRow - beginRow;
-  sheet.getRange(beginRow, 4, rows, cols).setBackgroundColor(colorName);  
-  color = !color;  
-//  Logger.log("beginRow: %s\tRows: %s\tcolor: %s", beginRow, rows, color);
-}
+      _.each(oddArray, function(e) {
+        ss.getRange(e).setBackgroundColor("#f3f3f3");
+    });
+    } // eof, alterBackgroundColor
+  }; // eof, public
+};
